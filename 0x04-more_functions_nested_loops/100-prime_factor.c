4b9a96e9216151fe57cb6a79d6f95f93778d7854
Benny254 <asshimbenny01@gmail.com>
2023-07-21 14:41:06 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 #include <math.h>
 
 /**
@@ -9,7 +9,7 @@
 int main(void)
 {
 	long int n;
-	long int maxi;
+	long int max;
 	long int i;
 
 	n = 612852475143;
@@ -21,7 +21,7 @@ int main(void)
 		n /=2;
 	}
 
-	for (i = 3; i <= sqrt(n); i = i +2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
 		{
