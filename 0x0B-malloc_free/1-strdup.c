9c3a2d245d968cc4fad31f43860c3934fe838e0c
Benny254 <asshimbenny01@gmail.com>
2023-08-07 01:58:02 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * _strup - dublicating to the new memory location
+ * _strdup - dublicating to the new memory location
  * @str: char
  * Return: 0
  */
