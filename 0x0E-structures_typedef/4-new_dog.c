1d941496bb0e5d28c0fa4b04128321c2de4bf27a
Benny254 <asshimbenny01@gmail.com>
2023-08-14 11:39:49 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * _strlen - to returns the length of a string
+ * _strlen -to returns the length of a string
  * @s: the string to be evaluated
  *
  * Return: the length of a string
@@ -22,20 +22,19 @@ int _strlen(char *s)
 }
 
 /**
- * *_strcpy - to copies the string pointed to by src
- * to include the terminating null byte (\0)
- * the buffer pointed to by dest
- * @dest:the pointer to the buffer in which we are to copy the string
+ * *_strcpy - to copy the string pointed to by src
+ * including the terminating null byte (\0)
+ * to the Buffer pointed to by dest
+ * @dest: A pointer to the buffer in which we copy the string
  * @src: the string to be copied
  *
- * Return: A pointer to the dest
+ * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
 	int len, a;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
@@ -51,10 +50,10 @@ char *_strcpy(char *dest, char *src)
 }
 
 /**
- * new_dog - To create a new dog
- * @name: The name of a dog
- * @age: The age of a dog
- * @owner: The owner of a dog
+ * new_dog - to creates a new dog
+ * @name: the name of a dog
+ * @age: Age of the dog
+ * @owner:the owner of the dog
  *
  * Return: A pointer to the new dog (Success), NULL otherwise
  */
@@ -77,4 +76,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
+
+
+
