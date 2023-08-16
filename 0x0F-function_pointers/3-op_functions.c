efdca893c91a18ab260bb0c2ae26204d24ec1738
Benny254 <asshimbenny01@gmail.com>
2023-08-16 04:58:56 -0700

    updated

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+int op_add(int c, int d);
+int op_sub(int c, int d);
+int op_mul(int c, int d);
+int op_div(int c, int d);
+int op_mod(int c, int d);
+
+/**
+ * op_add - to return the sum of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The sum of c and d.
+ */
+int op_add(int c, int d)
+{
+	return (c + d);
+}
+/**
+ * op_sub - to return the difference of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The difference of c and d.
+ */
+int op_sub(int c, int d)
+{
+	return (c - d);
+}
+/**
+ * op_mul - to return the product of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The product of c and d.
+ */
+int op_mul(int c, int d)
+{
+	return (c * d);
+}
+/**
+ * op_div - to return the division of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The quotient of c and d.
+ */
+int op_div(int c, int d)
+{
+	return (c / d);
+}
+/**
+ * op_mod - to return the remainder of the division of two numbers.
+ * @a: first number.
+ * @b: second number.
+ *
+ * Return: The remainder of the division of c by d.
+ */
+int op_mod(int c, int d)
+{
+	return (c % d);
+}
