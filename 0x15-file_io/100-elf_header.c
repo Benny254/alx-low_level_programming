0b70bf501dbde74d273acda33566a4459a16b79b
Benny254 <asshimbenny01@mail.com>
2023-10-02 13:27:47 -0700

    update

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -8,10 +8,10 @@
 #include <sys/types.h>
 #include <sys/uio.h>
 
-int create_file(const char *filename, char *text_content);
-ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
+ssize_t read_textfile(const char *filename, size_t letters);
 int _strlen(char *s);
+int create_file(const char *filename, char *text_content);
 
 #endif
