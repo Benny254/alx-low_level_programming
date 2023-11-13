b5e05ac49575fe6a85275306d67041e828eee424
Benny254 <asshimbenny01@mail.com>
2023-11-13 03:40:33 -0800

    update

---
+++ 0x18-dynamic_libraries/0-main.h
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - to check the code for alx students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("Alx"));
+    return (EXIT_SUCCESS);
+}
