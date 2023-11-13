2178e2c22d85d11e24c8c4e7a102b2f00358c22d
Benny254 <asshimbenny01@mail.com>
2023-11-13 07:03:51 -0800

    update

---
+++ 0x18-dynamic_libraries/functions/all.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+ * add - to add two integers
+ * @a: first unit
+ * @b: second unit
+ * Return: addition
+ */
+int add(int a, int b)
+{
+        return(a + b);
+}
+
+/**
+ * sub - to subtract two integers
+ * @w: first unit
+ * @q: second unit
+ * Return: subtraction
+ */
+int sub(int w, int q)
+{
+        return(w - q);
+}
+
+/**
+ * mul - to multiple two integers
+ * @c: first unit
+ * @d: second unit
+ * Return: multiplication
+ */
+int mul(int c, int d)
+{
+        return(c * d);
+}
+/**
+ * div - to divide two integers
+ * @m: first unit
+ * @n: second unit
+ * Return: division
+ */
+int div(int m, int n)
+{
+        return(m / n);
+}
+
+/**
+ * mod - to modulus two integers
+ * @a: first unit
+ * @b: second unit
+ * Return: modulation
+ */
+int mod(int a, int b)
+{
+        return(a % b);
+}
